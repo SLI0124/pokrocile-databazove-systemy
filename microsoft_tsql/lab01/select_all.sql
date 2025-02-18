d7183e7044a0a89565f15f2732b15326b1eb3de6
Vojtech Sliva <sli0124@vsb.cz>
2025-02-18 22:59:47 +0100

    chore(task1): remove select count sql files

--- microsoft_tsql/lab01/select_all.sql
+++
@@ -1,6 +0,0 @@
-select (select count(*) from customer)  as customer_count,
-       (select count(*) from [Order])   as order_count,
-       (select count(*) from OrderItem) as order_item_count,
-       (select count(*) from Product)   as product_count,
-       (select count(*) from staff)     as staff_count,
-       (select count(*) from store)     as store_count;
--- oracle_plsql/lab01/select_all.sql
+++
@@ -1,7 +0,0 @@
-SELECT (SELECT COUNT(*) FROM customer)  AS customer_count,
-       (SELECT COUNT(*) FROM "Order")   AS order_count,
-       (SELECT COUNT(*) FROM OrderItem) AS order_item_count,
-       (SELECT COUNT(*) FROM Product)   AS product_count,
-       (SELECT COUNT(*) FROM staff)     AS staff_count,
-       (SELECT COUNT(*) FROM store)     AS store_count
-FROM dual;
