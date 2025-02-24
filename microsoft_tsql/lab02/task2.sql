be0ef4ded2de160a6bc2804ac64502bd9d27a8c1
Vojtech Sliva <sli0124@vsb.cz>
2025-02-24 22:37:21 +0100

    feat(task2): WIP

---
+++ microsoft_tsql/lab02/task2.sql
@@ -0,0 +1,82 @@
+-- 2.1.2
+-- index_customer
+exec PrintIndexes 'Customer';
+go
+-- index_order
+exec PrintIndexes 'Order';
+go
+-- index_orderitem
+exec PrintIndexes 'OrderItem';
+go
+-- index_product
+exec PrintIndexes 'Product';
+go
+-- index_staff
+exec PrintIndexes 'Staff';
+go
+-- index_store
+exec PrintIndexes 'Store'
+go
+
+-- 2.1.2
+-- Customer
+-- index_blocks_customer
+exec PrintPagesIndex 'PK__Customer__D0587687B018A16B';
+
+-- heap_blocks_order
+exec PrintPagesHeap 'Customer';
+
+-- Order
+-- index_blocks_order
+exec PrintPagesIndex 'PK__Order__C8AAF6FEB772B1E2';
+
+-- heap_blocks_order
+exec PrintPagesHeap 'Order';
+
+-- OrderItem
+-- index_blocks_orderitem
+exec PrintPagesIndex 'PK__OrderIte__CD4431637FC271FD';
+
+-- heap_blocks_orderitem
+exec PrintPagesHeap 'OrderItem';
+
+-- Product
+-- index_blocks_product
+exec PrintPagesIndex 'PK__Product__5EEC79D0EED0C505';
+
+-- heap_blocks_product
+exec PrintPagesHeap 'Product';
+
+-- Staff
+-- index_blocks_staff
+exec PrintPagesIndex 'PK__Staff__98C886A82049A895';
+
+-- heap_blocks_staff
+exec PrintPagesHeap 'Staff';
+
+-- Store
+-- index_blocks_store_v1
+exec PrintPagesIndex 'PK__Store__A4B61B1120307B18';
+
+-- heap_blocks_store
+exec PrintPagesHeap 'Store';
+
+-- 2.2.1
+-- Order
+-- first
+select i.name, s.index_depth - 1 as height, sum(s.page_count) as page_count
+from sys.dm_db_index_physical_stats(DB_ID(N'sli0124'), OBJECT_ID(N'Order'), NULL, NULL, 'DETAILED') s
+         join sys.indexes i on s.object_id = i.object_id and s.index_id = i.index_id
+where name = 'PK__Order__C8AAF6FEB772B1E2'
+group by i.name, s.index_depth
+
+-- second
+select s.index_level                              as level,
+       s.page_count,
+       s.record_count,
+       s.avg_record_size_in_bytes                 as avg_record_size,
+       round(s.avg_page_space_used_in_percent, 1) as page_utilization,
+       round(s.avg_fragmentation_in_percent, 2)   as avg_frag
+from sys.dm_db_index_physical_stats(DB_ID(N'sli0124'), OBJECT_ID(N'Order'), NULL, NULL, 'DETAILED') s
+         join sys.indexes i on s.object_id = i.object_id and s.index_id = i.index_id
+where name = 'PK__Order__C8AAF6FEB772B1E2';
---
+++ oracle_plsql/lab02/task2.sql
@@ -0,0 +1,135 @@
+-- 2.1.1
+-- index_customer
+select index_name
+from USER_INDEXES
+where TABLE_NAME = 'CUSTOMER';
+
+-- index_order
+select index_name
+from USER_INDEXES
+where TABLE_NAME = 'Order';
+
+-- index_orderitem
+select index_name
+from USER_INDEXES
+where TABLE_NAME = 'ORDERITEM';
+
+-- index_product
+select index_name
+from USER_INDEXES
+where TABLE_NAME = 'PRODUCT';
+
+-- index_staff
+select index_name
+from USER_INDEXES
+where TABLE_NAME = 'STAFF';
+
+-- index_store
+select index_name
+from USER_INDEXES
+where TABLE_NAME = 'STORE';
+
+-- 2.1.2
+-- using user_segments(user_tables) - small output, not usable
+-- index_blocks_customer_v1
+select blocks
+from USER_SEGMENTS
+where SEGMENT_NAME = 'CUSTOMER';
+
+-- index_blocks_order_v1
+select blocks
+from USER_SEGMENTS
+where SEGMENT_NAME = 'Order';
+
+-- index_blocks_orderitem_v1
+select blocks
+from USER_SEGMENTS
+where SEGMENT_NAME = 'ORDERITEM';
+
+-- index_blocks_product_v1
+select blocks
+from USER_SEGMENTS
+where SEGMENT_NAME = 'PRODUCT';
+
+-- index_blocks_staff_v1
+select blocks
+from USER_SEGMENTS
+where SEGMENT_NAME = 'STAFF';
+
+-- index_blocks_store_v1
+select blocks
+from USER_SEGMENTS
+where SEGMENT_NAME = 'STORE';
+
+-- using dbms_space.unused_space - that is what we want
+-- Customer
+-- index_blocks_customer_v2a
+call PRINTPAGES_UNUSED_SPACE('CUSTOMER', 'SLI0124', 'TABLE');
+
+-- heap_blocks_customer_v2a
+call PRINTPAGES('CUSTOMER', 'SLI0124');
+
+-- Order
+-- index_blocks_order_v2a
+call PRINTPAGES_UNUSED_SPACE('Order', 'SLI0124', 'TABLE');
+
+-- heap_blocks_order_v2a
+call PRINTPAGES('Order', 'SLI0124');
+
+-- OrderItem
+-- index_blocks_orderitem_v2a
+call PRINTPAGES_UNUSED_SPACE('ORDERITEM', 'SLI0124', 'TABLE');
+
+-- heap_blocks_orderitem_v2a
+call PRINTPAGES('ORDERITEM', 'SLI0124');
+
+-- Product
+-- index_blocks_product_v2a
+call PRINTPAGES_UNUSED_SPACE('PRODUCT', 'SLI0124', 'TABLE');
+
+-- heap_blocks_product_v2a
+call PRINTPAGES('PRODUCT', 'SLI0124');
+
+-- Staff
+-- index_blocks_staff_v2a
+call PRINTPAGES_UNUSED_SPACE('STAFF', 'SLI0124', 'TABLE');
+
+-- heap_blocks_staff_v2a
+call PRINTPAGES('STAFF', 'SLI0124');
+
+-- Store
+-- index_blocks_store_v2a
+call PRINTPAGES_UNUSED_SPACE('STORE', 'SLI0124', 'TABLE');
+
+-- heap_blocks_store_v2a
+call PRINTPAGES('STORE', 'SLI0124');
+
+-- using dbms_space.space_usage - different output, not usable
+-- index_blocks_customer_v2b
+call PRINTPAGES_SPACE_USAGE('CUSTOMER', 'SLI0124', 'TABLE');
+
+-- index_blocks_order_v2b
+call PRINTPAGES_SPACE_USAGE('ORDER', 'SLI0124', 'TABLE');
+
+-- index_blocks_orderitem_v2b
+call PRINTPAGES_SPACE_USAGE('ORDERITEM', 'SLI0124', 'TABLE');
+
+-- index_blocks_product_v2b
+call PRINTPAGES_SPACE_USAGE('PRODUCT', 'SLI0124', 'TABLE');
+
+-- index_blocks_staff_v2b
+call PRINTPAGES_SPACE_USAGE('STAFF', 'SLI0124', 'TABLE');
+
+-- index_blocks_store_v2b
+call PRINTPAGES_SPACE_USAGE('STORE', 'SLI0124', 'TABLE');
+
+-- 2.2.1.
+-- Order
+ANALYZE INDEX SYS_C0020808 VALIDATE STRUCTURE;
+
+select height-1 as h, blocks, lf_blks as leaf_pages, br_blks as inner_pages, lf_rows as leaf_items,
+       br_rows as inner_items, pct_used
+from index_stats where name='SYS_C0020808';
+
+-- 2.3.1.
+CREATE INDEX idx_customer_lName ON CUSTOMER (lName);
