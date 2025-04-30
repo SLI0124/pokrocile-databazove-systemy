431b5c0d25dfc6319e071bda43e0f27b5aec6515
Vojtěch Slíva <sli0124@vsb.cz>
2025-04-30 10:11:06 +0200

    fix(task9): correct various mistakes from initial source

--- cpp/cSpRTreeNode.h
+++ cpp/cSpRTreeNode.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <cfloat>
 
 #include "cDynamicMemory.h"
 #include "cVector.h"
--- cpp/cVectorDb.h
+++ cpp/cVectorDb.h
@@ -6,7 +6,7 @@
 #include <sstream>
 #include <string>
 
-#include "cUniformRandomdouble.h"
+#include "cUniformRandomDouble.h"
 
 using namespace std;
 
